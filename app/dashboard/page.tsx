f60dd8b93819647313306347fc78fd480dbc0712
Nathaniel Essien <thaniessy@gmail.com>
2025-11-14 20:12:45 +0100

    Fix text size and button padding on dashboard for improved layout consistency.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
       <div className=" m-[20px] lg:m-[50px] flex flex-col gap-[40px]">
       <DashboardNav />
         <div className="bg-[#272829] lg:h-[52px] p-[24px] rounded-[15px] flex-col lg:flex-row flex lg:items-center lg:justify-between gap-[24px] lg:gap-0">
-          <h2 className="text-[20pc] font-bold">
+          <h2 className="text-[20px] font-bold">
             Welcome to your dashboard David - you are highly favoured
           </h2>
           <div className="flex gap-4 items-center">
@@ -18,7 +18,7 @@ const Page = () => {
               <Bell size={26} />
               <span className="w-2 h-2 rounded-full bg-[#4de209] absolute top-0 right-1"></span>
             </div>
-            <div className="bg-[#4de209] rounded-full px-6 py-2 flex items-center justify-center text-black font-bold gap-2">
+            <div className="bg-[#4de209] rounded-full px-4 h-[30px] flex items-center justify-center text-black font-bold gap-2">
               <p className="">Create new listing</p>
               <Power size={18} className="font-bold" />
             </div>
