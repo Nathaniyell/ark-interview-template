0b6724a47f0c2be58686c9f8d6a3b88d47debe3b
Nathaniel Essien <thaniessy@gmail.com>
2025-11-14 22:56:41 +0100

    Update DashboardNav layout by changing list justification to improve visual consistency and user experience.

--- components/dashboard/dashboard-nav.tsx
+++ components/dashboard/dashboard-nav.tsx
@@ -22,7 +22,7 @@ export function DashboardNav() {
   return (
     <header className="w-full h-[97px] bg-[#272829] text-white rounded-[12px] pb-[24px] pt-[20px] px-[20px] shadow-lg">
       <nav className="bg-[#3a3b3c] lg:h-[32px] rounded-[10px] overflow-x-auto flex items-center">
-        <ul className="flex flex-row items-center justify-between gap-[10px] flex-nowrap min-w-max mx-auto">
+        <ul className="flex flex-row items-center justify-evenly gap-[10px] flex-nowrap min-w-max mx-auto">
           {navLinks.map((link) => (
             <li
               key={link.title}
